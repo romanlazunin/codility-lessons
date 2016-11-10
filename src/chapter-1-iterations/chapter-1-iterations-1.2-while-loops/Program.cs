c50dbdaa288fe4354607b82fdccc3f341ad4cd9c
Roman Lazunin <rlazunin@gmail.com>
2016-11-10 21:13:41 +0100

    added project 1.2 while loops and WhileLoops class and tests

---
+++ src/chapter-1-iterations/chapter-1-iterations-1.2-while-loops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter_1_iterations_1._2_while_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 3;
+
+            Console.WriteLine(WhileLoop.Run(n));
+        }
+    }
+}
---
+++ src/chapter-1-iterations/chapter-1-iterations-1.2-while-loops/WhileLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter_1_iterations_1._2_while_loops
+{
+    public class WhileLoop
+    {
+        public static string Run(int n)
+        {
+            var result = string.Empty;
+            var i = 0;
+            while (i < n)
+            {
+                result += '*';
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
