1452a13377a1c18326e6ab4dab90b3db3ff61a55
Roman Lazunin <rlazunin@gmail.com>
2016-11-10 16:34:22 +0100

    added solution chapter-1-interations and project chapter-1-iterations-1

---
+++ src/chapter-1-iterations/chapter-1-iterations/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter_1_iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chapter-1 - iterations - example 1");
+        }
+    }
+}
