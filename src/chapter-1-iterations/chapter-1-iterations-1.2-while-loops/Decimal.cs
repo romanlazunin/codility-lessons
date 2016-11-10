abb08146e546792bb37e39701ead8f9a6a188ac2
Roman Lazunin <rlazunin@gmail.com>
2016-11-10 23:14:24 +0100

    added 'while loop' class and tests

---
+++ src/chapter-1-iterations/chapter-1-iterations-1.2-while-loops/Decimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter_1_iterations_1._2_while_loops
+{
+    public class Decimal
+    {
+        public static int CountDigits(int n)
+        {
+            var count = 1;
+
+            return count;
+        }
+    }
+}
---
+++ test/chapter-1-iterations/chapter-1-iterations-1.2-while-loops.test/DecimalTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace chapter_1_iterations_1._2_while_loops.test
+{
+    [TestClass]
+    public class DecimalTest
+    {
+        [TestMethod]
+        public void DecimalTest_CountDigits_TestMethod1()
+        {
+            var n = 0;
+            var expected = 1;
+            var actual = Decimal.CountDigits(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecimalTest_CountDigits_TestMethod2()
+        {
+            var n = 1;
+            var expected = 1;
+            var actual = Decimal.CountDigits(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecimalTest_CountDigits_TestMethod3()
+        {
+            var n = 2;
+            var expected = 1;
+            var actual = Decimal.CountDigits(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecimalTest_CountDigits_TestMethod4()
+        {
+            var n = 3;
+            var expected = 1;
+            var actual = Decimal.CountDigits(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
