5336c0ff4e172f91d94ab8c541db4de8f91dd05a
Lazunin Roman <roman.lazunin@afconsult.com>
2016-11-16 10:51:31 +0100

    added folder and solution for codility-trial-tasks and task 1 with unit tests

---
+++ src/codility-trial-tasks/codility-trial-tasks/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codility_trial_tasks
+{
+    public class Solution
+    {
+        public int Run(int[] A)
+        {
+            int n = A.Length;
+            int[] L = new int[n + 1];
+            L[0] = -1;
+            for (int i = 0; i < n; i++)
+            {
+                L[i + 1] = A[i];
+            }
+            int count = 0;
+            int pos = (n + 1) / 2;
+            int candidate = L[pos];
+            for (int i = 1; i <= n; i++)
+            {
+                if (L[i] == candidate)
+                    count = count + 1;
+            }
+            if (count > pos)
+                return candidate;
+            return (-1);
+        }
+    }
+}
---
+++ test/codility-trial-tasks.test/codility-trial-tasks.test/UnitTest2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace codility_trial_tasks.test
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
