1bb409701270c6c80ae7ab65aaab36c181c07979
Roman Lazunin <rlazunin@gmail.com>
2016-11-16 16:50:13 +0100

    added folders and projects for 'pit depth' trial task

---
+++ src/codility-trial-tasks/codility-trial-tasks-pit-depth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codility_trial_tasks_pit_depth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ test/codility-trial-tasks-pit-depth.test/codility-trial-tasks-pit-depth.test/SolutionTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace codility_trial_tasks_pit_depth.test
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Solution_Run_Test1()
+        {
+            Assert.AreNotEqual(true, false);
+        }
+    }
+}
