485e0c58ce5d1e0cab6a29f8cd69793b5a1b2d29
Roman Lazunin <rlazunin@gmail.com>
2016-11-16 17:11:48 +0100

    added some prototype code for 'pit depth' trial task solution

--- src/codility-trial-tasks/codility-trial-tasks-pit-depth/Program.cs
+++ src/codility-trial-tasks/codility-trial-tasks-pit-depth/Program.cs
@@ -10,6 +10,33 @@ namespace codility_trial_tasks_pit_depth
     {
         static void Main(string[] args)
         {
+            //var A = new int[] { };
+            //var A = new int[] { 0 };
+            //var A = new int[] { 0, 0 };
+            //var A = new int[] { 0, 0, 0 };
+            var A = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            
+            if (A.Length < 3)
+                Console.WriteLine(-1);
+
+            int count = 0;
+
+            for (int i = 0; i < A.Length - 2; i++)
+            {
+                for (int j = i + 1; j < A.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < A.Length; k++)
+                    {
+                        count++;
+                        //Console.Write(A[i] + " ");
+                        //Console.Write(A[j] + " ");
+                        //Console.Write(A[k] + " ");
+                        //Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
---
+++ src/codility-trial-tasks/codility-trial-tasks-pit-depth/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codility_trial_tasks_pit_depth
+{
+    public class Solution
+    {
+        public int Run(int[] A)
+        {
+            if (A.Length < 3)
+                return -1;
+
+            for (int i = 0; i < A.Length - 2; i++)
+            {
+                Console.Write(A[i] + " ");
+                for (int j = i + 1; j < A.Length - 1; j++)
+                {
+                    Console.Write(A[j] + " ");
+                    for (int k = j + 1; k < A.Length; k++)
+                    {
+                        Console.Write(A[k] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            return -1;
+        }
+    }
+}
