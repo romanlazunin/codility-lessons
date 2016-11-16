22ef17b74fb783322e5060c3ce4fdd1826e05a1e
Roman Lazunin <rlazunin@gmail.com>
2016-11-16 17:36:31 +0100

    changes to prototype code for 'pit depth' trial task solution

--- src/codility-trial-tasks/codility-trial-tasks-pit-depth/Program.cs
+++ src/codility-trial-tasks/codility-trial-tasks-pit-depth/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,19 +15,46 @@ namespace codility_trial_tasks_pit_depth
             //var A = new int[] { 0 };
             //var A = new int[] { 0, 0 };
             //var A = new int[] { 0, 0, 0 };
-            var A = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            //var A = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            var A = new int[] { 0, 1, 3, -2, 0, 1, 0, -3, 2, 3 };
             
             if (A.Length < 3)
                 Console.WriteLine(-1);
 
             int count = 0;
-
+            int depth = 0;
+                        
             for (int i = 0; i < A.Length - 2; i++)
             {
                 for (int j = i + 1; j < A.Length - 1; j++)
                 {
                     for (int k = j + 1; k < A.Length; k++)
                     {
+                        bool truePit = true;
+
+                        for (int m = i; m < j; m++)
+                        {
+                            if (!(A[m] > A[m + 1]))
+                            {
+                                truePit = false;
+                            }
+                        }
+
+                        for (int m = j; m < k; m++)
+                        {
+                            if (!(A[m] < A[m + 1]))
+                            {
+                                truePit = false;
+                            }
+                        }
+
+                        if (!truePit)
+                        {
+                            continue;
+                        }
+
+                        var currentDepth = Math.Min(A[i] - A[j], A[k] - A[j]);
+                        depth = Math.Max(depth, currentDepth);
                         count++;
                         //Console.Write(A[i] + " ");
                         //Console.Write(A[j] + " ");
@@ -37,6 +65,7 @@ namespace codility_trial_tasks_pit_depth
             }
 
             Console.WriteLine(count);
+            Console.WriteLine(depth);
         }
     }
 }
